0c75ae1cb567dddc0a7399b0c090384343f5df5a
Crazybiophys <123protasovaelen@gmail.com>
2022-04-24 12:49:18 +0000

    checking if directory exists

--- check_disk_usage.sh
+++ check_disk_usage.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 dir="$1"
-echo -e "Checking disk usage in $dir\n"
+if [ -d $dir ]; then
+    echo -e "Checking disk usage in $dir\n"
+else
+    echo "$dir does not exist, existing."
+    exit 101
+fi
