a208a0cf4f66480b3c9a7565be229c7d54f6a252
Crazybiophys <123protasovaelen@gmail.com>
2022-04-24 13:57:14 +0000

    determing of the heaviest subfolder

--- check_disk_usage.sh
+++ check_disk_usage.sh
@@ -6,3 +6,8 @@ else
     echo "$dir does not exist, existing."
     exit 101
 fi
+
+echo -e "The heaviest subfolder of $dir is:"
+top1=$(du -S $dir | sort -rn | head -n 1 | awk '{print $2 "\n"}')
+disk=$(du -h $top1 | awk '{print $1}')
+echo -e "$top1 with disk usage of $disk"
